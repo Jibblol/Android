3ee7ee8b4fb2e6dca7a09920c92f1b5850a8afe8
jibblol <jibblol@gmail.com>
2017-05-27 16:14:43 +0200

    we shall pee

---
+++ Hotellapp/app/src/main/java/com/example/vegard/hotellapp/LateArrivalActivity.java
@@ -0,0 +1,13 @@
+package com.example.vegard.hotellapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LateArrivalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_late_arrival);
+    }
+}
